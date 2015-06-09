8ec81030f5e6a08c48084c5b7f19a71d6a69b71b
stephen williams <stephen.ray.williams@gmail.com>
2015-06-09 03:24:12 -0500

    trying out jsoup. seems easier to get started with

---
+++ src/main/java/com/monco/calendarofveeshan/RaidPhpParser.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.monco.calendarofveeshan;
+
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+/**
+ *
+ * @author stephen.williams@monco.info
+ */
+public class RaidPhpParser {
+
+    final String URL = "https://www.project1999.com/raid.php";
+
+    public void crawlTest(String url) throws IOException {
+        if ((url == null) || (url == "")) {
+            url = URL;
+        }
+        Document doc = Jsoup.connect(url).get();
+        Elements newsHeadlines = doc.select("#mp-itn b a");
+        String output = "\nCrawlTest Headlines:\n";
+        output += newsHeadlines.text();
+        System.out.println(output);
+    }
+
+    public void crawl(String url) throws IOException {
+        if ((url == null) || (url == "")) {
+            url = URL;
+        }
+        Document doc = Jsoup.connect(url).get();
+        Elements newsHeadlines = doc.select("#mp-itn b a");
+        String output = "\nHeadlines:\n";
+        output += newsHeadlines.text();
+        System.out.println(output);
+    }
+
+    public static void main(String[] args) throws IOException {
+        RaidPhpParser rpp = new RaidPhpParser();
+        rpp.crawlTest("http://en.wikipedia.org/");
+    }
+}
